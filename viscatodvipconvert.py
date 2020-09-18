c7f2fbe8441dde0dbb4311eef5a1c2084e65b415
gigaplant <64315978+gigaplant@users.noreply.github.com>
2020-09-18 12:14:38 +0100

    Update viscatodvipconvert.py
    Cleaned up code and added comments where to put camera IP address

--- viscatodvipconvert.py
+++ viscatodvipconvert.py
@@ -1,6 +1,6 @@
 import socket
 
-CAMIPADDR1 = '192.168.100.81'
+CAMIPADDR1 = '192.168.100.81' #Enter Camera IP address here
 PORTNUM = 1259
 
 socketreceive = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -8,9 +8,7 @@ socketreceive.bind(('127.0.0.1',PORTNUM))
 while True:
 	data, addr = socketreceive.recvfrom(1024)
 	print("received message: %s" % data)
-	#print("length: %s" % len(data))
 	rawdatalen = len(data)
-	#print (type(data))
 	frame = b""
 	frame += b'\x00' 
 	frame += bytes.fromhex(format(rawdatalen+2, '02x'))
